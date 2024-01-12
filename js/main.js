c15bea62eab84f912013dd805c3f23068676132f
ariannacostanzo <costanzo.arianna@outlook.it>
2024-01-12 15:46:54 +0100

    finishing point 1

--- js/main.js
+++ js/main.js
@@ -6,25 +6,41 @@ const logSomething = (something) => console.log(something);
 // * 4. I valori che l'utente inserisce negli input devono essere confrontati con i numeri casuali generati
 // * 5. In base agli input dell'utente faccio apparire in pagina un messaggio relativo al risultato
 
+//!Raccolgo tutti gli elementi della pagina che mi servono
+const numbersContainer = document.getElementById('numbers-container');
+
+//!----------------------------
+//!funzioni
+//!----------------------------
+
 const generateRandomNumbers = () => {
 
     const randomNumbers = [];
     
     while (randomNumbers.length < 5) {
         
+        //Genero numeri finchè l'array non ne ha 5 diversi tra loro
         const randomNumber = Math.floor(Math.random() * 100) +1;
-        
+
         if (!randomNumbers.includes(randomNumber)) {
             randomNumbers.push(randomNumber);
             
+            //creo 5 <li> che avranno come testo il numero casuale del ciclo e queste le appendo all'<ul>
+            //lo metto all'interno dell'if altrimenti mostrerei in pagina anche i numeri che sono ripetuti
+            const listElement = document.createElement('li');
+            listElement.innerText = randomNumber;
+            numbersContainer.appendChild(listElement);
         }
-
     }
-    logSomething(randomNumbers)
 
-    
+    logSomething(randomNumbers)
     return randomNumbers;
 }
 
+//!-----------------------------
+//!SVOLGIMENTO DELLA LOGICA
+//!-----------------------------
 
+//All'avvio della pagina genero 5 numeri casuali diversi (potrei mettere un button che fa iniziare tutto)
 generateRandomNumbers();
+
