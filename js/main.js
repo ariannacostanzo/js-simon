791e615dcb33c7b2858d9dc39446c67364fb1383
ariannacostanzo <costanzo.arianna@outlook.it>
2024-01-12 15:26:10 +0100

    implementing schedule

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-console.log('js ok')
+const logSomething = (something) => console.log(something);
+
+// * 1. Generare 5 numeri casuali da 1 a 100 e stamparli in pagina
+// * 2. Creare un timer che parte da 30 e finisce a 0 e mostrarlo in pagina
+// * 3. Quando il timer arriva a 0 devo fare sparire i numeri e fare comparire 5 input
+// * 4. I valori che l'utente inserisce negli input devono essere confrontati con i numeri casuali generati
+// * 5. In base agli input dell'utente faccio apparire in pagina un messaggio relativo al risultato
